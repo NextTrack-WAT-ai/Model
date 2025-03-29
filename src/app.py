4449d3a904ab8fe1ad70d3c0e79dcc37581a8f44
Nathan Chu <nathan.ch.chu@gmail.com>
2025-03-29 17:18:07 -0400

    module import fix

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from flask import Flask, request, jsonify
 from reshuffler import shufflePlaylist, load_dataset
 import pandas as pd
